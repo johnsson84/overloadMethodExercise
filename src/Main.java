925cb54b138f98d2597c0de74456a9e9369ca17d
Johan Johnsson <johan@johnsson-net.se>
2023-09-20 09:18:34 +0200

    Added first method that converts inches to cm.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class Main {
+
+    static double convertMethod(int inches) {
+        double cm = inches * 2.54;
+        return cm;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Please input how many inches: ");
+        System.out.println("Inches in cm is: " + convertMethod(input.nextInt()));
     }
 }
