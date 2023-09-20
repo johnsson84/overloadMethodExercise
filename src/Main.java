d516a585712afa05f611782c4ad3ad0dcc478a6c
Johan Johnsson <johan@johnsson-net.se>
2023-09-20 09:48:58 +0200

    Added second method to enter height and the convert back to inches.

--- src/Main.java
+++ src/Main.java
@@ -7,10 +7,26 @@ public class Main {
         return cm;
     }
 
+    static double convertMethod(int meters, int centimeters) {
+        int cm = centimeters;
+        for (int i = 0; i < meters; i++) {
+            cm += 100;
+        }
+        return ((convertMethod(cm) / 2.54) / 2.54);
+    }
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
+        System.out.println("METHOD 1");
         System.out.print("Please input how many inches: ");
         System.out.println("Inches in cm is: " + convertMethod(input.nextInt()));
+
+        System.out.println("\nMETHOD 2");
+        System.out.println("Please enter your height. First meters then centimeters");
+        System.out.print("Meters: "); int m = input.nextInt();
+        System.out.print("Centimeters: "); int cm = input.nextInt();
+        System.out.println("Your height converted to inches is: " + convertMethod(m, cm));
+
     }
 }
